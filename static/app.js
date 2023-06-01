a8f9155985b8d8a38ea6d74443e2e92f80dd21bb
atrconic <ana.trconic@gmail.com>
2023-06-01 17:17:05 +0200

    crocodile game

---
+++ static/app.js
@@ -0,0 +1,8 @@
+function hello(){
+    console.log("hello!!!")
+    $('#myClass').removeClass("back-ground-light")
+    $('#myClass').addClass("back-ground-dark")
+}
+
+$(document).ready(function() {
+});
